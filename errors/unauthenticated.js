76f9024086c8c19cdd25798f5f0243d81296a94f
hxnshaw <eghosasere07@gmail.com>
2023-01-03 19:12:11 +0100

    errors

---
+++ errors/unauthenticated.js
@@ -0,0 +1,11 @@
+const CustomError = require("./custom-api");
+const { StatusCodes } = require("http-status-codes");
+
+class UnauthenticatedError extends CustomError {
+  constructor(message) {
+    super(message);
+    this.statusCode = StatusCodes.UNAUTHORIZED;
+  }
+}
+
+module.exports = UnauthenticatedError;
